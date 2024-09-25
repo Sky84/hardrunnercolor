3fd666bc0f88ba42b19788c22e77ab30d85f8677
Etienne SC <etienne.salupocorvo@gmail.com>
2024-09-25 18:04:41 +0200

    feat lerp wip

--- player.gd
+++ player.gd
@@ -5,29 +5,38 @@ extends Node2D
 @onready var green_ray_cast_2d: RayCast2D = $Raycasts/GreenRayCast2D4
 @onready var red_ray_cast_2d: RayCast2D = $Raycasts/RedRayCast2D2
 
+@onready var tween: Tween = get_tree().create_tween().bind_node(self).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE); 
+
+class RaycastColor:
+	var raycast: RayCast2D;
+	var color: Color;
+	func _init(raycast: RayCast2D, color: Color) -> void:
+		self.raycast = raycast
+		self.color = color
+
 
 #declaring dictionary that mapping the roation and raycasts
-var _ray_casts = {
-	0: yellow_ray_cast_2d,
-	90: blue_ray_cast_2d,
-	180: green_ray_cast_2d,
-	270: red_ray_cast_2d
+@onready var _ray_casts = {
+	0: RaycastColor.new(yellow_ray_cast_2d, Color.YELLOW),
+	90: RaycastColor.new(blue_ray_cast_2d, Color.BLUE),
+	180: RaycastColor.new(green_ray_cast_2d, Color.GREEN),
+	270: RaycastColor.new(red_ray_cast_2d, Color.RED)
 }
 
 var _current_color: Color;
+func _process(delta):
+	for raycast_key_rotation in _ray_casts:
+		var raycast_color: RaycastColor = _ray_casts[raycast_key_rotation];
+		if raycast_color.raycast.is_colliding() and _current_color != raycast_color.color:
+			_on_collision_raycast(raycast_key_rotation, raycast_color);
+			print(raycast_color.raycast.get_collider().name);
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass 
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_collision_raycast(desired_rotation: int, _raycast_color: RaycastColor) -> void:
+	_current_color = _raycast_color.color;
 
 # when player press the screen or the mouse button, rotate the player at 90 degrees relative to the current roation
 func _input(event: InputEvent) -> void:
-	if event is InputEventScreenTouch or event is InputEventMouseButton:
-		rotation_degrees = fmod(rotation_degrees + 90, 360);
-		
- 
+	if (event is InputEventScreenTouch or event is InputEventMouseButton) and event.is_pressed():
+		var desired_rotation = fmod(rotation_degrees + 90, 360);
+		#tween the rotation of the player
+		tween.tween_property(self, "rotation_degrees", desired_rotation, 0.5);
